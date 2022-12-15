acdbc73d525e804d789bf5ef7ad44f62b1f4e969
sagebeme <eudicas5k@gmail.com>
2022-12-15 07:31:17 -0800

    creating a function to print the character and strings from stacks

--- opcodes2.c
+++ opcodes2.c
@@ -1,78 +1,15 @@
 #include "monty.h"
 
 /**
- * _div - divides the second element by the top element of the stack
- *
- * @doubly: head of the linked list
- * @cline: line number;
- * Return: no return
- */
-void _div(stack_t **doubly, unsigned int cline)
-{
-	int m = 0;
-	stack_t *aux = NULL;
-
-	aux = *doubly;
-
-	for (; aux != NULL; aux = aux->next, m++)
-		;
-
-	if (m < 2)
-	{
-		dprintf(2, "L%u: can't div, stack too short\n", cline);
-		free_vglo();
-		exit(EXIT_FAILURE);
-	}
-
-	if ((*doubly)->n == 0)
-	{
-		dprintf(2, "L%u: division by zero\n", cline);
-		free_vglo();
-		exit(EXIT_FAILURE);
-	}
-
-	aux = (*doubly)->next;
-	aux->n /= (*doubly)->n;
-	_pop(doubly, cline);
-}
-
-/**
- * _mul - multiplies the top element to the second top element of the stack
- *
- * @doubly: head of the linked list
- * @cline: line number;
- * Return: no return
- */
-void _mul(stack_t **doubly, unsigned int cline)
-{
-	int m = 0;
-	stack_t *aux = NULL;
-
-	aux = *doubly;
-
-	for (; aux != NULL; aux = aux->next, m++)
-		;
-
-	if (m < 2)
-	{
-		dprintf(2, "L%u: can't mul, stack too short\n", cline);
-		free_vglo();
-		exit(EXIT_FAILURE);
-	}
-
-	aux = (*doubly)->next;
-	aux->n *= (*doubly)->n;
-	_pop(doubly, cline);
-}
-
-/**
  * _mod - computes the rest of the division of the second element
  * by the top element of the stack
- *
  * @doubly: head of the linked list
  * @cline: line number;
  * Return: no return
+ * info:The result is stored in 2nd value node from the top
+ * and top value removed.
  */
+
 void _mod(stack_t **doubly, unsigned int cline)
 {
 	int m = 0;
@@ -102,6 +39,28 @@ void _mod(stack_t **doubly, unsigned int cline)
 	_pop(doubly, cline);
 }
 /**
+ * _pstr - prints the string of the stack
+ *
+ * @doubly: head of the linked list
+ * @cline: line number;
+ * Return: no return
+ */
+void _pstr(stack_t **doubly, unsigned int cline)
+{
+	stack_t *aux;
+	(void)cline;
+
+	aux = *doubly;
+
+	while (aux && aux->n > 0 && aux->n < 128)
+	{
+		printf("%c", aux->n);
+		aux = aux->next;
+	}
+
+	printf("\n");
+}
+/**
  * _pchar - print the char value of the first element
  *
  * @doubly: head of the linked list
@@ -123,27 +82,4 @@ void _pchar(stack_t **doubly, unsigned int cline)
 		exit(EXIT_FAILURE);
 	}
 	printf("%c\n", (*doubly)->n);
-}
-
-/**
- * _pstr - prints the string of the stack
- *
- * @doubly: head of the linked list
- * @cline: line number;
- * Return: no return
- */
-void _pstr(stack_t **doubly, unsigned int cline)
-{
-	stack_t *aux;
-	(void)cline;
-
-	aux = *doubly;
-
-	while (aux && aux->n > 0 && aux->n < 128)
-	{
-		printf("%c", aux->n);
-		aux = aux->next;
-	}
-
-	printf("\n");
 }
