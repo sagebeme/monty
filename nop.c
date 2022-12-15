69f67edba939461e82f2a05718b53d4708d94de9
sagebeme <eudicas5k@gmail.com>
2022-12-15 05:55:56 -0800

    doesn't do anything

---
+++ nop.c
@@ -0,0 +1,13 @@
+#include "monty.h"
+
+/**
+ *_nop - does not do anything
+ *@stack: double pointer that point to the head of stack
+ *@line_number: the number of line in file
+ *
+ */
+void _nop(stack_t **stack, unsigned int line_number)
+{
+	(void) stack;
+	(void) line_number;
+}
