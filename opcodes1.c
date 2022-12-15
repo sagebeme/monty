0f3692fbcc5538c57b20dadb5cf70acb0ea1175b
sagebeme <eudicas5k@gmail.com>
2022-12-15 07:29:31 -0800

    working on comments

--- opcodes1.c
+++ opcodes1.c
@@ -1,35 +1,11 @@
 #include "monty.h"
 
 /**
- * _queue - sets format of data to queue(FIFI/LILO)
- * @doubly: head of linked list
- * @cline: line number
- */
-void _queue(stack_t **doubly, unsigned int cline)
-{
-	(void)doubly;
-	(void)cline;
-
-	vglo.lifo = 0;
-}
-
-/**
- * _stack - sets format of data to stack (LIFO/FILO)
- * @doubly: linked list head
- * @cline: line number
- */
-void _stack(stack_t **doubly, unsigned int cline)
-{
-	(void)doubly;
-	(void)cline;
-
-	vglo.lifo = 1;
-}
-
-/**
- * _add - add the top 2 elements of the stack
+ * _add - Adds the top two values of a stack_t linked list.
  * @doubly: head of the linked list
  * @cline: line number
+ * Description: The result is stored in the second value node
+ * from the top and the top value  is removed.
  */
 void _add(stack_t **doubly, unsigned int cline)
 {
@@ -63,13 +39,44 @@ void _nop(stack_t **doubly, unsigned int cline)
 	(void)doubly;
 	(void)cline;
 }
+/**
+ * _sub - Subtracts 2nd value from the top of stack_t linked list by top value.
+ * @head: the head node
+ * @cline: the line number
+ * Description: The result is stored in the second value node
+ * from the top and the top value is removed.
+ */
+
+void _sub(stack_t **head, unsigned int cline)
+{
+	int m = 0;
+	stack_t *current;
+
+	current = *head;
+
+	for (; current != NULL; current = current->next, m++)
+		;
+
+	if (m < 2)
+	{
+		dprintf(2, "L%u: can't sub, stack too short\n", cline);
+		free_vglo();
+		exit(EXIT_FAILURE);
+	}
+
+	current = (*head)->next;
+	current->n -= (*head)->n;
+	_pop(head, cline);
+}
 
 /**
- * _sub - subtracts top 2 elements of the stack
- * @doubly:linked list head
- * @cline: line number
+ *_div -Divides 2nd value from the top of stack_t linkedlist by the top value.
+ * @doubly: head of the linked list
+ * @cline: line number;
+ * Return: no return
  */
-void _sub(stack_t **doubly, unsigned int cline)
+
+void _div(stack_t **doubly, unsigned int cline)
 {
 	int m = 0;
 	stack_t *aux = NULL;
@@ -81,12 +88,49 @@ void _sub(stack_t **doubly, unsigned int cline)
 
 	if (m < 2)
 	{
-		dprintf(2, "L%u: can't sub, stack too short\n", cline);
+		dprintf(2, "L%u: can't div, stack too short\n", cline);
+		free_vglo();
+		exit(EXIT_FAILURE);
+	}
+
+	if ((*doubly)->n == 0)
+	{
+		dprintf(2, "L%u: division by zero\n", cline);
 		free_vglo();
 		exit(EXIT_FAILURE);
 	}
 
 	aux = (*doubly)->next;
-	aux->n -= (*doubly)->n;
+	aux->n /= (*doubly)->n;
 	_pop(doubly, cline);
+}
+
+/**
+ *_mul -Multiplies 2nd value from the top of stack_t linked list by top value.
+ * @head: the head node
+ * @cline: the line number
+ * info:The result is stored in the 2nd value node
+ * from the top and top value removed.
+ */
+
+void _mul(stack_t **head, unsigned int cline)
+{
+	int m = 0;
+	stack_t *current;
+
+	current = *head;
+
+	for (; current != NULL; current = current->next, m++)
+		;
+
+	if (m < 2)
+	{
+		dprintf(2, "L%u: can't mul, stack too short\n", cline);
+		free_vglo();
+		exit(EXIT_FAILURE);
+	}
+
+	current = (*head)->next;
+	current->n *= (*head)->n;
+	_pop(head, cline);
 }
